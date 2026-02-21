8c579fd7c02b7da289bfa9bccf43c2fcfc36fb33
Julio Flores <julio.cesar.2000@hotmail.com>
2026-02-21 13:10:57 -0500

    Updated project

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,28 @@
-//  @ts-check
-
+// @ts-check
+import { defineConfig, globalIgnores } from 'eslint/config'
 import { tanstackConfig } from '@tanstack/eslint-config'
 
-export default [...tanstackConfig]
+export default defineConfig([
+  globalIgnores([
+    '.output/',
+    'dist/',
+    'node_modules/',
+    '.nitro/',
+    '.generated/',
+  ]),
+
+  ...tanstackConfig,
+
+  {
+    rules: {
+      '@typescript-eslint/consistent-type-imports': 'off',
+      '@typescript-eslint/array-type': 'off',
+      '@typescript-eslint/naming-convention': 'off',
+      '@typescript-eslint/no-unnecessary-condition': 'off',
+      '@typescript-eslint/no-unnecessary-type-assertion': 'off',
+      'sort-imports': 'off',
+      'import/order': 'off',
+      'import/consistent-type-specifier-style': 'off',
+    },
+  },
+])
