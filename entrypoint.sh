ecfe1d1dd3677abbe8aba4efa7119fac8785c282
v4nishp01nt <shagasresthaa@gmail.com>
2026-04-03 12:07:42 -0400

    replaced the favicon, added the docker files and the entrypoint scripts. Will implement CI/CD once the initial deployment is done

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Setup script for environment variables from Docker Secrets inside the container
+#set -e
+
+export DATABASE_URL=$(cat /run/secrets/db_url)
+export BETTER_AUTH_URL=$(cat /run/secrets/better_auth_url)
+export BETTER_AUTH_SECRET=$(cat /run/secrets/better_auth_secret)
+export GOOGLE_CLIENT_ID=$(cat /run/secrets/google_client_id)
+export GOOGLE_CLIENT_SECRET=$(cat /run/secrets/google_client_secret)
+
+exec "$@"
